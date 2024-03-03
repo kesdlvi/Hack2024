6b043532de1395f689d80b427437201d3336bef2
jacque <jn21263@g.ucla.edu>
2024-03-03 12:20:36 -0800

    working on random

---
+++ placetoplace/src/app/randomize/page.js
@@ -0,0 +1,26 @@
+const Randomize = () => {
+    return ( 
+    <div className='region-filter'>
+        <h1 className='page-title'>Choose A Region</h1>
+            <div className='regions'>
+                <h2>Anywhere</h2>
+                <img className= 'all' src="https://t4.ftcdn.net/jpg/03/06/49/25/360_F_306492512_Cnv1Ie8ZZ1qWMhtFJAupzafY9DGf5Bi8.jpg" alt="" />
+                <h2>Africa</h2>
+                <img className= 'africa' src="https://static.vecteezy.com/system/resources/previews/003/087/809/non_2x/outline-simple-map-of-africa-free-vector.jpg" alt="" />
+                <h2>Asia</h2>
+                <img className= 'asia' src="https://gisgeography.com/wp-content/uploads/2023/12/Asia-Countries-Blank-Map.jpg" alt="" />
+                <h2>Austrailia</h2>
+                <img className= 'aussie' src="https://i.pinimg.com/564x/55/a0/1f/55a01f624fbd1036b4090d2f6a846ea5.jpg" alt="" />
+            </div>
+            <div className='regions'>
+                <h2>Europe</h2>
+                <img className= 'europe' src="https://static.vecteezy.com/system/resources/previews/003/087/855/original/outline-simple-map-of-europe-free-vector.jpg" alt="" />
+                <h2>North America</h2>
+                <img className= 'northam' src="https://upload.wikimedia.org/wikipedia/commons/4/49/Outline_North_America_%28PSF%29.png" alt="" />
+                <h2>South America</h2>
+                <img className= 'southam' src="https://static.vecteezy.com/system/resources/previews/003/087/842/original/outline-simple-map-of-south-america-free-vector.jpg" alt="" />
+            </div>
+    </div>
+    ) 
+}
+export default Randomize;
