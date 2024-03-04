761b3cef8a140a254c1a57aa1eb0e8ccd48069e1
jacque <jn21263@g.ucla.edu>
2024-03-03 17:33:59 -0800

    hyun edit for buttons

--- placetoplace/src/app/randomize/page.js
+++ placetoplace/src/app/randomize/page.js
@@ -1,6 +1,6 @@
 import '../(styles)/randomize.css';
 
-const Randomize = () => {
+const randomize = () => {
     return ( 
     <>
         <h1 className='page-title'>Choose A Region</h1>
@@ -9,7 +9,7 @@ const Randomize = () => {
         <h2>Anywhere</h2>
         </div>
         <div className='top-region'>
-            <img className='all' src="https://t4.ftcdn.net/jpg/03/06/49/25/360_F_306492512_Cnv1Ie8ZZ1qWMhtFJAupzafY9DGf5Bi8.jpg" alt="" />
+            <button className= {"btn btn-shadow-drop btn-shadow-drop--black"}><img className='all' width = "680" src="https://t4.ftcdn.net/jpg/03/06/49/25/360_F_306492512_Cnv1Ie8ZZ1qWMhtFJAupzafY9DGf5Bi8.jpg" alt="" /></button>
         </div>
         <div className='names'>
         <h2>Africa</h2>
@@ -21,11 +21,13 @@ const Randomize = () => {
         <div className='names'>
         </div> */}
         <div className='middle-region'>
-          <img className='img' src="https://static.vecteezy.com/system/resources/previews/003/087/809/non_2x/outline-simple-map-of-africa-free-vector.jpg" alt="" />
-          <img className='img' src="https://gisgeography.com/wp-content/uploads/2023/12/Asia-Countries-Blank-Map.jpg" alt="" />
-          <img className='img' src="https://i.pinimg.com/564x/55/a0/1f/55a01f624fbd1036b4090d2f6a846ea5.jpg" alt="" />
+            <button className= {"btn btn-shadow-drop btn-shadow-drop--black"}><img className='img' src="https://static.vecteezy.com/system/resources/previews/003/087/809/non_2x/outline-simple-map-of-africa-free-vector.jpg" alt="" /></button>
+            <button className= {"btn btn-shadow-drop btn-shadow-drop--black"}><img className='img' src="https://gisgeography.com/wp-content/uploads/2023/12/Asia-Countries-Blank-Map.jpg" alt="" /></button>
+            <button className= {"btn btn-shadow-drop btn-shadow-drop--black"}><img className='img' src="https://i.pinimg.com/564x/55/a0/1f/55a01f624fbd1036b4090d2f6a846ea5.jpg" alt="" /></button>
         </div>
         <div className='names'>
+
+            
         <h2>Europe</h2>
         <div className='space2'> <h2>North America</h2> </div>
         <div className='space3'> <h2>South America</h2> </div>
@@ -35,9 +37,9 @@ const Randomize = () => {
         <div className='names'>
         </div>
         <div className='bottom-region'>
-          <img className='img' src="https://static.vecteezy.com/system/resources/previews/003/087/855/original/outline-simple-map-of-europe-free-vector.jpg" alt="" />
-          <img className='img' src="https://upload.wikimedia.org/wikipedia/commons/4/49/Outline_North_America_%28PSF%29.png" alt="" />
-          <img className='img' src="https://static.vecteezy.com/system/resources/previews/003/087/842/original/outline-simple-map-of-south-america-free-vector.jpg" alt="" />
+            <button className= {"btn btn-shadow-drop btn-shadow-drop--black"}><img className='img' src="https://static.vecteezy.com/system/resources/previews/003/087/855/original/outline-simple-map-of-europe-free-vector.jpg" alt="" /></button>
+            <button className= {"btn btn-shadow-drop btn-shadow-drop--black"}><img className='img' src="https://upload.wikimedia.org/wikipedia/commons/4/49/Outline_North_America_%28PSF%29.png" alt="" /></button>
+            <button className= {"btn btn-shadow-drop btn-shadow-drop--black"}><img className='img' src="https://static.vecteezy.com/system/resources/previews/003/087/842/original/outline-simple-map-of-south-america-free-vector.jpg" alt="" /></button>
         </div>
       
     </>
@@ -45,4 +47,4 @@ const Randomize = () => {
     );
   }
   
-  export default Randomize;
+  export default randomize;
