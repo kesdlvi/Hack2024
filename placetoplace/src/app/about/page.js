2e8a60250711a36d415d8e5a486ef73aa84da2c0
Jason Lozada <jasonlozada16@g.ucla.edu>
2024-03-03 20:09:18 -0800

    Added an about me

---
+++ placetoplace/src/app/about/page.js
@@ -0,0 +1,11 @@
+const About = () => {
+return (
+<>
+<h1>
+    Our app aims to demonstrate that travel is more than just a journey from one destination to another. Rather, it is an opportunity to expand on cultural understanding and celebrate diversity. In a world often divided by differences, we're on a mission to bridge cultural gaps and build wonderful experiences from spaces all over the world, one traveler at a time
+</h1>
+</>
+);
+}
+
+export default About;
